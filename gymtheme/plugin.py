7cd76f08a2ff5a92216d8a8c7f230a44ef6e875b
Roman Edirisinghe <rediris@users.noreply.github.com>
2023-06-07 15:33:33 -0400

    Use JSON to generate footer links

--- gymtheme/plugin.py
+++ gymtheme/plugin.py
@@ -1,21 +1,29 @@
 import pkg_resources
+import requests
 
 from tutor import hooks
 
 from .__about__ import __version__
 
+response = requests.get('https://data.gym.soy/feeds/complete.json')
+if response.status_code == 200:
+    data = response.json()
+else:
+    # Print an error message
+    print('Error fetching data')
 
 ################# Configuration
 config = {
     # Add here your new settings
     "defaults": {
         "VERSION": __version__,
-        "WELCOME_MESSAGE": "Free online courses on design and development",
+        "WELCOME_MESSAGE": data['items']['config']['meta']['subtitle'],
         "PRIMARY_COLOR": "#0077c8",  # Aquent Gymnasium Blue
+        "SECONDARY_COLOR": "#ff5f14",  # Aquent Gymnasium Orange
         # Footer links are dictionaries with a "title" and "url"
         # To remove all links, run:
         # tutor config save --set GYM_FOOTER_NAV_LINKS=[] --set GYM_FOOTER_LEGAL_LINKS=[]
-        "FOOTER_NAV_LINKS": [],
+        "FOOTER_NAV_LINKS": data['items']['config']['navigation']['footer'],
         "FOOTER_LEGAL_LINKS": [],
     },
     "unique": {},
