a704018da455ebff2c819a2b2efa051b80617229
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-03-28 12:05:10 -0400

    decouple dependency

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     python_requires=">=3.8",
-    install_requires=["tutor>=17.0.0,<18.0.0", "gym-mfe>=17.0.0,<18.0.0"],
+    install_requires=["tutor>=17.0.0,<18.0.0"],
     extras_require={"dev": "tutor[dev]>=17.0.0,<18.0.0"},
     entry_points={"tutor.plugin.v1": ["gym-theme = gymtheme.plugin"]},
     classifiers=[
