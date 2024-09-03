3e0a6f15b285f713e00cdb899e65716885cbe965
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-09-03 14:52:28 -0400

    force waffle activation of courseware sidebar nav

--- gymtheme/templates/gym-theme/tasks/init.sh
+++ gymtheme/templates/gym-theme/tasks/init.sh
@@ -14,3 +14,14 @@ assign_theme('{{ CMS_HOST }}:8001')
 assign_theme('{{ PREVIEW_LMS_HOST }}')
 assign_theme('{{ PREVIEW_LMS_HOST }}:8000')
 "
+
+# MFE-specific tasks
+./manage.py lms waffle_flag --list > /tmp/lms_waffle_flags.txt
+
+# Force enable `courseware.enable_navigation_sidebar`
+{% if is_mfe_enabled("learning") %}
+grep courseware.enable_navigation_sidebar /tmp/lms_waffle_flags.txt || ./manage.py lms waffle_flag --create --everyone courseware.enable_navigation_sidebar
+{% else %}
+./manage.py lms waffle_delete --flags \
+    courseware.enable_navigation_sidebar
+{% endif %}
