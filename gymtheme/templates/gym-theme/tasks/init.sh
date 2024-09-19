b09a8bc02fbad00399fa0e2d08d0d41cf204e8d6
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-09-19 15:19:57 -0400

    fix: update init.sh, automatically enable theme

--- gymtheme/templates/gym-theme/tasks/init.sh
+++ gymtheme/templates/gym-theme/tasks/init.sh
@@ -1,18 +1,32 @@
-# Assign themes only if no other theme exists yet
+# Assign theme automatically via @https://github.com/aulasneo/tutor-contrib-branding/blob/master/tutorbranding/templates/tasks/lms/init
 ./manage.py lms shell -c "
 import sys
 from django.contrib.sites.models import Site
-def assign_theme(domain):
+def assign_theme(name, domain):
+    print('Assigning theme', name, 'to', domain)
+    if len(domain) > 50:
+            sys.stderr.write(
+                'Assigning a theme to a site with a long (> 50 characters) domain name.'
+                ' The displayed site name will be truncated to 50 characters.\\n'
+            )
     site, _ = Site.objects.get_or_create(domain=domain)
-    if not site.themes.exists():
-        site.themes.create(theme_dir_name='gym-theme')
-
-assign_theme('{{ LMS_HOST }}')
-assign_theme('{{ LMS_HOST }}:8000')
-assign_theme('{{ CMS_HOST }}')
-assign_theme('{{ CMS_HOST }}:8001')
-assign_theme('{{ PREVIEW_LMS_HOST }}')
-assign_theme('{{ PREVIEW_LMS_HOST }}:8000')
+    if not site.name:
+        name_max_length = Site._meta.get_field('name').max_length
+        site.name = domain[:name_max_length]
+        site.save()
+    site.themes.all().delete()
+    if name != 'default':
+        site.themes.create(theme_dir_name=name)
+domain_names = [
+    '{{ LMS_HOST }}',
+    '{{ CMS_HOST }}',
+    '{{ PREVIEW_LMS_HOST }}',
+    '{{ LMS_HOST }}:8000',
+    '{{ CMS_HOST }}:8001',
+    '{{ PREVIEW_LMS_HOST }}:8000',
+]
+for domain_name in domain_names:
+    assign_theme('gym-theme', domain_name)
 "
 
 # MFE-specific tasks
